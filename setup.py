4abe85e724d9900bd1e4435e15ea5eda1177e941
Tristan Carel <tristan@cogniteev.com>
2015-07-27 09:58:14 +0200

    PEP8 compliant

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
+
 def get_version(version_tuple):
     if not isinstance(version_tuple[-1], int):
         return '.'.join(map(str, version_tuple[:-1])) + version_tuple[-1]
