6091cffba1d932f752c40daa14f7c47016537475
Tristan Carel <tristan@cogniteev.com>
2015-07-27 09:00:03 +0200

    Build HTML coverage report

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ import os
 import sys
 from setuptools import setup, find_packages
 
-# Hack to silence atexit traceback in new
-
-
 def get_version(version_tuple):
     if not isinstance(version_tuple[-1], int):
         return '.'.join(map(str, version_tuple[:-1])) + version_tuple[-1]
@@ -25,41 +22,11 @@ LONG_DESCRIPTION = ""
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
+    'License :: OSI Approved :: Apache Software License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.2",
-    "Programming Language :: Python :: 3.3",
-    "Programming Language :: Python :: 3.4",
-    "Programming Language :: Python :: Implementation :: CPython",
-    "Programming Language :: Python :: Implementation :: PyPy",
 ]
-
-extra_opts = {"packages": find_packages(exclude=["tests", "tests.*"])}
-if sys.version_info[0] == 3:
-    extra_opts['use_2to3'] = True
-    extra_opts['tests_require'] = [
-        'nose', 'coverage==3.7.1',
-        'blinker', 'Pillow>=2.0.0'
-    ]
-    if "test" in sys.argv or "nosetests" in sys.argv:
-        extra_opts['packages'] = find_packages()
-        extra_opts['package_data'] = {
-            "tests": ["fields/mongoengine.png", "fields/mongodb_leaf.png"]
-        }
-else:
-    # coverage 4 does not support Python 3.2 anymore
-    extra_opts['tests_require'] = [
-        'nose', 'coverage==3.7.1',
-        'blinker', 'Pillow>=2.0.0',
-        'python-dateutil'
-    ]
-    if sys.version_info[0] == 2 and sys.version_info[1] == 6:
-        extra_opts['tests_require'].append('unittest2')
 
 setup(
     name='easy_upgrade',
@@ -70,7 +37,7 @@ setup(
     maintainer_email="tristan@{nospam}cogniteev.com",
     url='https://github.com/cogniteev/easy_upgrade',
     download_url='https://github.com/cogniteev/easy_upgrade/tarball/master',
-    license='MIT',
+    license='Apache license version 2.0',
     include_package_data=True,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
@@ -82,6 +49,8 @@ setup(
         'PyYAML>=3.11',
         'requests>=2.7.0',
     ],
+    tests_require=[
+    ],
     entry_points="""
         [console_scripts]
         easy_upgrade = easy_upgrade.cli:run
@@ -90,6 +59,5 @@ setup(
         stow = easy_upgrade.lib.stow
         [easy_upgrade.providers]
         github = easy_upgrade.lib.github:GitHubProvider
-    """,
-    **extra_opts
+    """
 )
